777f869cc7f7864d100a1fca88c3431f779f6620
unknown <may.alex43@gmail.com>
2018-08-03 13:59:33 -0400

    I think I met mvp, answered Review questions

--- Review.md
+++ Review.md
@@ -1,17 +1,24 @@
 # Review Questions
 
 ## What is Node.js?
+A runtime environment to run other programs for js apps outside of the browser.
 
 ## What is Express?
+A web app framework on top of the node.js web server. Adds functioinality like routing and middleware support.
 
-## Mention two parts of Express that you learned about this week.
+## Mention two parts of Express that you learned about this week. 
+Middleware and Resources
 
 ## What is Middleware?
+Software bridge between a database and applications
 
 ## What is a Resource?
+Abstraction of information. Any info that can be named
 
 ## What can the API return to help clients know if a request was successful?
 
 ## How can we partition our application into sub-applications?
+express and subapps
 
 ## What is express.json() and why do we need it?
+A middlware for Express. 
--- server.js
+++ server.js
@@ -1,12 +1,114 @@
 const express= require ('express');
+const actions= require('./data/helpers/actionModel');
+const projects= require('./data/helpers/projectModel');
 const server=express();
 
-server.use(express.json);
-
-server.listen(7000,() =>{
-    console.log('Server is listening on Port:7000')
-});
+server.use(express.json());
 
 server.get('/', (req,res)=> {
     res.send('Node-Express Sprint Challenge');
+});
+
+///////////////// Projects ///////////////////
+
+server.get('/projects', (req, res)=> {
+    projects.get()
+    .then(project=> {
+        res.json(project)
+    })
+    .catch(()=> {
+        res.status(500).json({error:'Could not retrieve projects'})
+    })
+})
+
+server.get('/projects/:id', (req, res)=>{
+    const {id}= req.params;
+    projects.get(id)
+    .then(project=>{
+        if(project===0){
+            res.status(404).json({message:'Project Id not found'})
+        } else {
+            res.json(project)
+        }
+    })
+    .catch(()=>{
+        res.status(500).json({error:'Project Id not found'})
+    })
+});
+
+server.get('/projects/actions/projectId', (req, res)=>{
+    const {projectid}= req.params;
+    projects.getProjectActions(projectid)
+    .then(project=>{
+        if(project===0){
+            res.status(404).json({message:'Project Id not found'})
+        } else {
+            res.json(project)
+        }
+    })
+    .catch(()=>{
+        res.status(500).json({error:'Project Id not found'})
+    })
+});
+
+server.post('/projects', (req, res)=> {
+    const{ name, description}= req.body;
+    if (!name|| !description){
+        res.status(400).json({errorMessage: 'Please provide name and description'})
+        return;
+    }
+    projects.insert({name, description})
+    .then(response =>{
+        res.status(201).json(response);
+    })
+    .catch(()=>{
+        res.status(500).json({error:'Error while posting new project'})
+    })
+});
+
+/////////////////// Actions ///////////////////
+
+server.get('/actions', (req, res)=> {
+    actions.get()
+    .then(action=>{
+        res.json(action)
+    })
+    .catch(()=> {
+        res.status(500).json({error:'Could not retrieve actions'})
+    })
+})
+
+server.get('/actions/:id', (req, res)=> {
+    const {id}= req.params;
+    actions.get()
+    .then(actions=>{
+        if (actions===0){
+            res.status(404).json({message:'Action Id not found'})
+        } else {
+            res.json(actions)
+        }
+    })
+    .catch(()=>{
+        res.status(500).json({error:'Action with specified Id could not be retrieved'})
+    })
+});
+
+server.post('/actions', (req, res)=> {
+    const{ project_id, description, notes}= req.body;
+    if (!project_id|| !description||notes){
+        res.status(400).json({errorMessage: 'Please provide a projectId, description and some notes'})
+        return;
+    }
+    actions.insert({project_id, description, notes})
+    .then(response =>{
+        res.status(201).json(response);
+    })
+    .catch(()=>{
+        res.status(500).json({error:'Error while posting new actions'})
+    })
+});
+
+
+server.listen(7000, () =>{
+    console.log('Server is listening on Port:7000')
 });
